d2221f52df06ae124c96a59e05fc67820a02121f
Jamie <jamie.ellesmere@gmail.com>
2017-01-14 10:58:11 +0000

    Modified files from previous commit

--- src/main/java/com/financeanalyser/view/FinanceAnalyserScene.java
+++ src/main/java/com/financeanalyser/view/FinanceAnalyserScene.java
@@ -1,11 +1,21 @@
 package com.financeanalyser.view;
 
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
 import javafx.scene.Scene;
 
 public class FinanceAnalyserScene extends Scene {
 
+	private static final String CSS_LOCATION = "/com/financeanalyser/css/style.css";
+
 	public FinanceAnalyserScene() {
 		super(new MainPane());
+		getStylesheets().add(loadStyle());
 	}
 
+	private String loadStyle() {
+		String s = getClass().getResource(CSS_LOCATION).toExternalForm();
+		System.err.println(s);
+		return s;
+	}
 }
