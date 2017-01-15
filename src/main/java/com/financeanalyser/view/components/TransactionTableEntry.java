cc0cf1e66166de3e2726c0898bc46e3978a37815
Jamie <jamie.ellesmere@gmail.com>
2017-01-15 20:39:49 +0000

    Updated transaction table

--- src/main/java/com/financeanalyser/view/components/TransactionTableEntry.java
+++
@@ -1,5 +0,0 @@
-package com.financeanalyser.view.components;
-
-public class TransactionTableEntry {
-
-}
--- src/main/java/com/financeanalyser/view/panes/RecordPane.java
+++ src/main/java/com/financeanalyser/view/panes/RecordPane.java
@@ -1,15 +1,19 @@
 package com.financeanalyser.view.panes;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.financeanalyser.model.data.Transaction;
+import com.financeanalyser.model.data.TransactionType;
 import com.financeanalyser.view.components.RecordsTable;
 import com.financeanalyser.view.viewswitchcontroller.FAViewSwitchController;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
 
 public class RecordPane extends AnchorPane {
@@ -28,6 +32,7 @@ public class RecordPane extends AnchorPane {
 
 	public RecordPane(FAViewSwitchController faViewSwitchController) {
 		super();
+		setAnchorPaneZero(this);
 		this.viewSwitchController = faViewSwitchController;
 
 		loadView();
@@ -36,7 +41,14 @@ public class RecordPane extends AnchorPane {
 	
 	private void initialiseFX() {
 		recordsTable = new RecordsTable();
+		setAnchorPaneZero(recordsTable);
 		recordTablePane.getChildren().add(recordsTable);
+		
+		Transaction transaction = new Transaction(LocalDate.now(), 950, TransactionType.CASH);
+		transaction.setName("Withdrawal");
+		transaction.setNote("Used for lunch.");
+		
+		recordsTable.getItems().add(transaction);
 	}
 
 	private void loadView() {
@@ -44,11 +56,19 @@ public class RecordPane extends AnchorPane {
 
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(MAIN_PANE_FXML_LOCATION));
 		loader.setController(this);
-		try {
+		try {	
 			AnchorPane pane = loader.load();
+			setAnchorPaneZero(pane);
 			super.getChildren().add(pane);
 		} catch (IOException e) {
 			LOG.error("Failed to load fxml for main pane.", e);
 		}
+	}
+
+	private void setAnchorPaneZero(Node pane) {
+		AnchorPane.setBottomAnchor(pane, 0.0);
+		AnchorPane.setLeftAnchor(pane, 0.0);
+		AnchorPane.setRightAnchor(pane, 0.0);
+		AnchorPane.setTopAnchor(pane, 0.0);
 	}
 }
