bf0e0b216edd43004b4e2bbb3f8984b97c466388
Stefan <slesnjakovic@gmail.com>
2017-01-15 20:48:21 +0000

    added record class to hold transactions

---
+++ src/main/java/com/financeanalyser/model/data/Record.java
@@ -0,0 +1,24 @@
+package com.financeanalyser.model.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Record {
+	private List<Transaction> records;
+
+	public Record() {
+		this.records = new ArrayList<>();
+	}
+
+	public List<Transaction> getRecord() {
+		return records;
+	}
+	
+	public boolean addTransaction(Transaction t) {
+		return records.add(t);
+		
+	}
+	
+	
+	
+}
